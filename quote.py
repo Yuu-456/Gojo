65361471854ed5ca9d047c3e5cf4530728de0f06
Izumi Tachibana <99785450+Yuu-456@users.noreply.github.com>
2022-05-18 22:19:59 +0900

    Create quote.py

---
+++ quote.py
@@ -0,0 +1,128 @@
+"""
+BSD 2-Clause License
+
+Copyright (C) 2017-2019, Paul Larsen
+Copyright (C) 2021-2022, Awesome-RJ, <https://github.com/Awesome-RJ>
+Copyright (c) 2021-2022, Yūki • Black Knights Union, <https://github.com/Awesome-RJ/CutiepiiRobot>
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+from io import BytesIO
+from traceback import format_exc
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from Cutiepii_Robot import arq
+from Cutiepii_Robot.utils.errors import capture_err
+from Cutiepii_Robot import pgram
+
+
+
+async def quotify(messages: list):
+    response = await arq.quotly(messages)
+    if not response.ok:
+        return [False, response.result]
+    sticker = response.result
+    sticker = BytesIO(sticker)
+    sticker.name = "sticker.webp"
+    return [True, sticker]
+
+
+def getArg(message: Message) -> str:
+    return message.text.strip().split(None, 1)[1].strip()
+
+
+def isArgInt(message: Message) -> list:
+    count = getArg(message)
+    try:
+        count = int(count)
+        return [True, count]
+    except ValueError:
+        return [False, 0]
+
+
+@pgram.on_message(filters.command("q") & ~filters.forwarded & ~filters.bot & ~filters.edited)
+@capture_err
+async def quotly_func(client, message: Message):
+    if not message.reply_to_message:
+        return await message.reply_text("Reply to a message to quote it.")
+    if not message.reply_to_message.text:
+        return await message.reply_text(
+            "Replied message has no text, can't quote it."
+        )
+    m = await message.reply_text("Quoting Messages Please wait....")
+    if len(message.command) < 2:
+        messages = [message.reply_to_message]
+
+    elif len(message.command) == 2:
+        arg = isArgInt(message)
+        if arg[0]:
+            if arg[1] < 2 or arg[1] > 10:
+                return await m.edit("Argument must be between 2-10.")
+            count = arg[1]
+            messages = await client.get_messages(
+                message.chat.id,
+                list(range(
+                        message.reply_to_message.message_id,
+                        message.reply_to_message.message_id + (count + 5),
+                    )),
+                replies=0,
+            )
+        else:
+            if getArg(message) != "r":
+                return await m.edit(
+                    "Incorrect Argument, Pass **'r'** or **'INT'**, **EX:** __/q 2__"
+                )
+            reply_message = await client.get_messages(
+                message.chat.id,
+                message.reply_to_message.message_id,
+                replies=1,
+            )
+            messages = [reply_message]
+    else:
+        return await m.edit(
+            "Incorrect argument, check quotly module in help section."
+        )
+    try:
+        sticker = await quotify(messages)
+        if not sticker[0]:
+            await message.reply_text(sticker[1])
+            return await m.delete()
+        sticker = sticker[1]
+        await message.reply_sticker(sticker)
+        await m.delete()
+        sticker.close()
+    except Exception as e:
+        await m.edit(
+            "Something wrong happened while quoting messages,"
+            + " This error usually happens when there's a "
+            + " message containing something other than text."
+        )
+        e = format_exc()
+        print(e)
+
+
+__mod_name__ = "Quotly"
